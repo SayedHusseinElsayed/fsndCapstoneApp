b8dd9195739731571b6bfb5a4c44f3b5877afa4b
sayedhussein <74625836+sayedhussein@users.noreply.github.com>
2021-05-11 03:34:14 +0200

    good changes

--- setup.sh
+++ setup.sh
@@ -8,4 +8,3 @@ export CLIENT_ID="438YU4jI85ibOZBrcviRlga34qTXVP76"
 export CALLBACK_URL="http://localhost:8100/tabs/user-page"
 export ALGORITHMS=['RS256']
 export CLIENT_SECRET="ZJQiCLpqm_xUKnjM8VkX-YwZcmqqB4xNvtKBb20PVJoEqb35GaSkI2m72VlTxWmw"
-export DATABASE_URL="postgresql:///castingagency"
